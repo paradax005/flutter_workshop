2ef6fb7ba85ea1fc485a4eefa821d48bc253034a
Borhen Ft <borhen.fatnassi@esprit.tn>
2022-12-29 23:12:03 +0100

    tabbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:workshop/screens/basket_screen.dart';
+import 'package:workshop/screens/login_screen.dart';
 import 'package:workshop/screens/navigation/home.dart';
 import 'package:workshop/screens/libray_screen.dart';
 import 'package:workshop/screens/product_info.dart';
 import 'package:workshop/screens/signup_screen.dart';
+
+import 'screens/navigation/tab_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,10 +26,13 @@ class MyApp extends StatelessWidget {
       ),
       routes: {
         '/': (BuildContext context) {
-          return const Home();
+          return const LoginScreen();
         },
         '/signup': (BuildContext context) {
           return const SignUpScreen();
+        },
+        '/tabbar': (BuildContext context) {
+          return const CustomTabBar();
         },
         '/home': (BuildContext context) {
           return const Home();
--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 class CustomDrawer extends StatelessWidget {
-  const CustomDrawer({super.key});
+  final int checkScreen;
+  const CustomDrawer({
+    super.key,
+    this.checkScreen = 0,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -22,12 +26,25 @@ class CustomDrawer extends StatelessWidget {
               title: const Text("Modifier le Profil"),
             ),
             ListTile(
-              onTap: () {},
-              leading: const Icon(
-                Icons.tab_outlined,
-                color: Colors.black,
-              ),
-              title: const Text("Navigation par onglet"),
+              onTap: () {
+                if (checkScreen != 0) {
+                  Navigator.pushReplacementNamed(context, '/home');
+                } else {
+                  Navigator.pushReplacementNamed(context, '/tabbar');
+                }
+              },
+              leading: checkScreen == 0
+                  ? const Icon(
+                      Icons.tab_outlined,
+                      color: Colors.black,
+                    )
+                  : const Icon(
+                      Icons.file_download_outlined,
+                      color: Colors.black,
+                    ),
+              title: checkScreen == 0
+                  ? const Text("Navigation par onglet")
+                  : const Text("Navigation du bas "),
             ),
           ],
         ),
